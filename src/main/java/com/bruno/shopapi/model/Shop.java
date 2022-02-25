2a6bb17ec6a8cfc884f3b72936ee137d49a50f04
brunoguerra <brunoguerra@hotmail.com>
2022-02-24 21:01:06 -0300

    Criando entidades e dto

---
+++ src/main/java/com/bruno/shopapi/model/Shop.java
@@ -0,0 +1,41 @@
+package com.bruno.shopapi.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.stream.Collectors;
+
+@Getter
+@Setter
+@Entity
+public class Shop {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String identifier;
+
+    private String status;
+
+    @Column(name = "date_shop")
+    private LocalDate dateShop;
+
+    @OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL, mappedBy = "shop")
+    private List<ShopItem> items;
+
+    public static Shop convert(ShopDTO shopDTO){
+        Shop shop = new Shop();
+        shop.setIdentifier(shopDTO.getIdentifier());
+        shop.setStatus(shopDTO.getStatus());
+        shop.setDateShop(shopDTO.getDateShop());
+        shop.setItems(shopDTO
+                .getItems()
+                .stream()
+                .map(i -> ShopItem.convert(i))
+                .collect(Collectors.toList())
+        );
+        return shop;
+    }
+}
